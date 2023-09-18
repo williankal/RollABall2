a7b0e4665aa09b1fa6f0055668389c9ec96285c7
williankal <kenzo.willian@hotmail.com>
2023-09-18 19:04:51 -0300

    tutorial end

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,68 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
 
-public class PlayerController : MonoBehaviour
+
+public class PlayerControler : MonoBehaviour
 {
+    private Rigidbody rb;
+    public float speed = 15;
+
+    public TextMeshProUGUI countText;
+    private int count;
+    
+    private float movementX;
+    private float movementY;
+
+    public GameObject winTextObject;
+    public GameObject loseTextObject;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        count = 7;
+        SetCountText();
+        winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue movementValue)
     {
-        
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText(){
+        countText.text = "Count: " + count.ToString();
+        if(count < 1)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp")) 
+        {
+            other.gameObject.SetActive(false);
+            count = count - 1;
+            SetCountText();
+        }
     }
 }
